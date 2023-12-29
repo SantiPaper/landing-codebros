a78f22b7a640fcaf85df3bbba87e1f890c2a9636
SantiPaper <santiagovitelli01@gmail.com>
2023-12-28 22:59:08 -0300

    style

--- landingpage/src/components/animation/style.ts
+++ landingpage/src/components/animation/style.ts
@@ -1,7 +1,17 @@
 import {styled} from 'styled-components'
 
 export const StyledAnimation = styled.div`
-width: 80%;
-background-color: var(--color);
+width: 90%;
+border: 5px solid white;
 height: 40rem;
+color: var(--color);
+display: flex;
+flex-direction: column;
+align-items: center;
+text-align: center;
+padding: 2rem;
+gap: 2rem;
+h3{
+    font-size: 4rem;
+}
 `
